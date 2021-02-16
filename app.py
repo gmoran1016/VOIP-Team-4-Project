a1b820e1b99ca3a48111fa2574a432deb626a8b0
gmoran1016@gmail.com <Cooper02-02>
2021-02-16 16:48:31 -0500

    Added comments and Status. Also changed Inputs again but still not working.

--- app.py
+++ app.py
@@ -7,40 +7,53 @@ from gtts import gTTS
 app = Flask(__name__)
 
 
+# Some Flask thing? IDK
 @app.route("/sms", methods=['GET', 'POST'])
+
+"""
+CURRENT STATUS:
+
+Program needs to wait for both the number and message to be received before calling start. This doesnt work currently.
+Currently will take any message sent to the number and convert it to a number and try to call it which doesnt work unless the message you send is a phone number.
+At which point it will just change the phone number to speech call successfully and then fail to read output.mp3.
+
+TODO:
+1. Get Inputs Working correctly
+2. Fix Output.mp3 twiml error
+3. Fix format code so number can be entered in any format
+4. Upload to cloud service for deployment
+5. Make sure it can handle multiple user at once. ( Probably can't do this at the moment)
+"""
+
+# gets incoming SMS and does the main stuff, Doesn't work fully yet
 def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
 
     # Start our TwiML response
     resp = MessagingResponse()
     print(body)
-    x = 0
-    while x < 2:
-        if 'number: ' in body:
-            numberString = body
-            number = numberString.removeprefix('number: ')
-            resp.message("calling {}... ".format(number))
-            x += 1
-            return number
-        elif 'message: ' in body:
-            resp.message("message")
-            message = body.removeprefix('message: ')
-            print('Message ' + message)
-            x+=1
-            return message
-        else:
-            resp.message("error ")
+    # supposed to send a text asking for number when any message is recived.
+    resp.message("Enter Target Phone number in format +15554443333")
+    # old number syntax can probably remove
+    number = body.removeprefix('number: ')
+    # Supposed to then send a text prompting for the message.
+    resp.message("Please enter the message to be converted")
+    # old Message syntax can probably remove
+    message = body.removeprefix('message: ')
+
+    # resp.message("calling {}... ".format(number))
     start(number, message)
     return str(resp)
 
 
+# supposed to call everything? IDK
 def start(number, message):
     tts(message)
     call(number)
 
 
+# Does the TTS Magic and saves as output.mp3 in root folder
 def tts(message):
     language = 'en'
 
@@ -48,6 +61,7 @@ def tts(message):
     output.save("output.mp3")
 
 
+# Does the calling
 def call(number):
     # Your Account Sid and Auth Token from twilio.com/console
     # and set the environment variables. See http://twil.io/secure
@@ -55,11 +69,14 @@ def call(number):
     auth_token = '0f7ca9a3e08adc10008de9151089f653'
     client = Client(account_sid, auth_token)
     print(number)
+    # makes sure number has US Code at beginning. Need to make this better in case of formatting issues
     if '+1' not in number:
         number = "+1" + number
-
+    # Creates the call client and twiml needed to call via Twilio
     call = client.calls.create(
-        twiml='<Response><Play>output.mp3</play></Response>',
+        twiml='<Response>'
+              '<Play>output.mp3</play>'
+              '</Response>',
         to=number,
         from_='+18283927597'
     )
