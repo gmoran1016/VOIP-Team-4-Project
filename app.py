9a3bb4c505bce1ea2e1aad8b4fc9f52e651ed3e1
Griffin Moran <gmoran1016@gmail.com>
2021-03-30 14:56:03 -0400

    Inputs now working

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ doesnt work unless the message you send is a phone number. At which point it wil
 speech call successfully and then fail to read output.mp3. 
 
 TODO:
-1. Get Inputs Working correctly
-2. Fix Output.mp3 twiml error
+1. Get Inputs Working correctly - Fixed
+2. Fix Output.mp3 twiml error - Fixed but file needs to be hosted somewhere
 3. Fix format code so number can be entered in any format
-4. Upload to cloud service for deployment
+4. Upload to cloud service for deployment - Have an idea for this so should be good
 5. Make sure it can handle multiple user at once. ( Probably can't do this at the moment)
+6. Figure out tts file hosting solution.
 """
 
 # Some Flask thing? IDK
@@ -30,33 +31,42 @@ def bot():
     msg = resp.message()
     
     #Convert number to correct format to pass to twilio
-    numberResp = "'"+incoming_msg+"'"
     
     #variable
     i =0
-    number = ''
-    message =''
+    
+    
     #Checks for the /s to start the program by asking for the number. Should increment through steps but currently doesnt
     if '/s' in incoming_msg:
         msg.body('Please enter a number in +15554443333 format:')
         print(resp)
-        if '+1' in numberResp:
-            number = numberResp
-            print('number')
-            msg.body('Please type /m then enter you message')
+    elif '+1' in incoming_msg:
+        
+        storeNumber(incoming_msg)
+        msg.body('Please type /m then enter you message')
             
-        elif '/m' in incoming_msg:
-            message = incoming_msg
-            print(message)
-            call(number,message)
-                
-
+    elif '/m' in incoming_msg:
+        storeMessage(incoming_msg)
+        call()       
     else:
         msg.body('Please Begin by sending /s')
 
     return str(resp)
 
+def storeNumber(number):
+    storeNumber.numberResp = "'"+number+"'"
+    print(storeNumber.numberResp)
 
+def storeMessage(message):
+    if '/m' in message:
+        print(message)
+        storeMessage.messageResp = message[2:]
+        print(storeMessage.messageResp)
+    else:
+        print('Error in message')
+       
+    
+    
 # Does the TTS Magic and saves as output.mp3 in root folder
 def tts(message):
     language = 'en'
@@ -66,24 +76,24 @@ def tts(message):
 
 
 # Does the calling
-def call(number,message):
+def call():
     # Your Account Sid and Auth Token from twilio.com/console
     # and set the environment variables. See http://twil.io/secure
     account_sid = 'AC75647f28d69ea248afba7325fc554d01'
     auth_token = '0f7ca9a3e08adc10008de9151089f653'
     client = Client(account_sid, auth_token)
-    print(number)
+    print(storeNumber.numberResp)
     # makes sure number has US Code at beginning. Need to make this better in case of formatting issues
     #Redundant Currently
-    if '+1' not in number:
-        number = "+1" + number
+    if '+1' not in storeNumber.numberResp:
+        storeNumber.numberResp = "+1" + storeNumber.numberResp
     # Creates the call client and twiml needed to call via Twilio
-    print('call time'+' '+number+' '+message)
+    print('call time'+' '+storeNumber.numberResp+' '+storeMessage.messageResp)
     call = client.calls.create(
         twiml='<Response>'
-              '<Say>'+message+'</Say>'
+              '<Say>'+storeMessage.messageResp+'</Say>'
               '</Response>',
-        to=number,
+        to=storeNumber.numberResp,
         from_='+18283927597'
     )
 
